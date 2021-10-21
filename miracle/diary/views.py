9ed2908958431950fb16502ea2b21131b58177ba
KeonHee Lee <power3869@gmail.com>
2021-10-21 23:41:12 +0900

    today_diary transport data through views.py to be shown in html

--- miracle/diary/views.py
+++ miracle/diary/views.py
@@ -1,12 +1,19 @@
+import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Diary
 
 def index(request):
     return render(request, 'diary/index.html')
 
 def today_dairy(request, username):
+    today = datetime.datetime.now()
+    today_humandate = today.strftime("%Y-%m-%d")
+    today_diary = Diary.objects.get(dt_created__date=today_humandate)
+    print(today_diary)
     context = {
-        "username":username
+        "username":username,
+        "today_diary":today_diary
     }
     return render(request, 'diary/today_diary.html', context=context)
 # Create your views here.
