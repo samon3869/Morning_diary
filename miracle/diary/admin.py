fc51d7e1e01aa39b309a437f833a3913b8a4badf
KeonHee Lee <power3869@gmail.com>
2021-10-28 12:32:34 +0900

    [Allauth: show User in admin_page]

--- miracle/diary/admin.py
+++ miracle/diary/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Diary
+from django.contrib.auth.admin import UserAdmin
+from .models import Diary, User
 # Register your models here.
+
 admin.site.register(Diary)
+admin.site.register(User, UserAdmin)
