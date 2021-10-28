e3bdbbef4cde31fcc1967d2ab9dc736a26d54039
KeonHee Lee <power3869@gmail.com>
2021-10-28 13:03:18 +0900

    [Allauth: Settings] modify Signup redirection url

--- miracle/diary/urls.py
+++ miracle/diary/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('index/', views.index),
+    path('index/', views.index, name='index'),
     path('today_diary/new/', views.today_diary_create, name='diary-create'),
     path('today_diary/<str:username>/', views.today_dairy, name='today-diary'),
 ]
--- miracle/miracle/settings.py
+++ miracle/miracle/settings.py
@@ -143,6 +143,9 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
+ACCOUNT_SIGNUP_REDIRECT_URL = "index"
+LOGIN_REDIRECT_URL = "index"
+
 # Email settings
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
