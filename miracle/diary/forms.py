d814d5090e387578d6c5635287f00f4f87f5f995
KeonHee Lee <power3869@gmail.com>
2021-10-29 12:33:26 +0900

    [Allauth: nickname field] append nickname field to SignupForm

--- miracle/diary/forms.py
+++ miracle/diary/forms.py
@@ -1,8 +1,18 @@
 from django import forms
-from .models import Diary
+from .models import Diary, User
 
 class DiaryForm(forms.ModelForm):
 
     class Meta:
         model = Diary
         exclude = ['dt_created', 'dt_modified']
+
+class SignupForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ["nickname"]
+
+    def signup(self, request, user):
+        user.nickname = self.cleaned_data["nickname"]
+        user.save()
