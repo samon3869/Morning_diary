0d41c2a80ef7466db0be0d33f5ad383b21e73409
KeonHee Lee <power3869@gmail.com>
2021-10-29 12:01:17 +0900

    [Allauth: User navbar] Change login method (username -> email)

--- miracle/diary/models.py
+++ miracle/diary/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return self.email
 
 
 class Diary(models.Model):
