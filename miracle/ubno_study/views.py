dad8e89b59fb670f8eea6c804b5233421d20e76e
KeonHee Lee <power3869@gmail.com>
2021-11-26 16:21:46 +0900

    [UBNO study] show dummy-data

--- miracle/ubno_study/views.py
+++ miracle/ubno_study/views.py
@@ -1,7 +1,16 @@
+import pandas as pd
+import os
 from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
 class MyStudyTime(View):
     def get(self, request, user_id):
-        return render(request, 'ubno_study/my_study_time.html')
+        module_dir = os.path.dirname(__file__) 
+        file_path = os.path.join(module_dir, 'static', 'ubno_study', 'dummydata', 'study_time_table.csv')
+        df = pd.read_csv(file_path)
+        df_for_html = df.to_html()
+        context = {
+            'df': df_for_html,
+        }
+        return render(request, 'ubno_study/my_study_time.html', context=context)
