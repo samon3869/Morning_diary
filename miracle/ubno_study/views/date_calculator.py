5f8a45e986d998eb445ad68c9323ea3a6b1b7276
KeonHee Lee <power3869@gmail.com>
2021-11-29 12:43:12 +0900

    [UBNO study] make date_calculator function

---
+++ miracle/ubno_study/views/date_calculator.py
@@ -0,0 +1,39 @@
+import datetime
+import pandas as pd
+import calendar
+
+# common function
+def add_days(source_date, count): 
+    target_date = source_date + datetime.timedelta(days = count) 
+    return target_date
+
+def date_range(start, end):
+    start = datetime.datetime(start.year, start.month, start.day)
+    end = datetime.datetime(end.year, end.month, end.day)
+    dates = [date for date in pd.date_range(start, periods=(end-start).days+1)]
+    return dates
+
+# week function
+def get_weeks_first_date(source_date): 
+    temporaryDate = datetime.datetime(source_date.year, source_date.month, source_date.day) 
+    weekDayCount = temporaryDate.weekday() 
+    target_date = add_days(temporaryDate, -weekDayCount)
+    return target_date
+
+def get_week_days(source_date):
+    first_date = get_weeks_first_date(source_date)
+    end_date = add_days(first_date, 6)
+    week_days = date_range(first_date, end_date)
+    return week_days
+
+# month function
+def get_month_days(source_date):
+    first_date = datetime.datetime(source_date.year, source_date.month, 1)
+    end_date = datetime.datetime(
+        source_date.year, 
+        source_date.month, 
+        calendar.monthrange(source_date.year, source_date.month)[1]
+    )
+    month_days = date_range(first_date, end_date)
+    return month_days
+
---
+++ miracle/ubno_study/views/handover_data_views.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import os
+import json
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+class MyStudyTime(View):
+    def get(self, request, user_id):
+        module_dir = os.path.dirname(__file__) 
+        file_path = os.path.join(module_dir, '..', 'static', 'ubno_study', 'dummydata', 'study_time_table.csv')
+        df = pd.read_csv(file_path)
+        
+        # 그래프 그리기용 테이블로 정리하기       
+        author = df['author'].unique()
+        table_for_graph = df.loc[df['author'] == author[0], ['dt_created']]
+        for i in range(len(author)):
+            dataset = df.loc[df['author'] == author[i], ['dt_created', 'minutes']]
+            dataset.rename(columns = {'minutes': author[i]}, inplace = True)
+            table_for_graph = pd.merge(table_for_graph, dataset, on='dt_created', how='outer')
+            
+        # 1. labels
+        labels = list(table_for_graph['dt_created'])
+        # color set
+        colors = ['#FF7F50', '#DE3163', '#9FE2BF',  '#FFBF00','#40E0D0', '#6495ED', '#DFFF00', '#CCCCFF']
+        # 2. datasets
+        datasets = []
+        column_length = table_for_graph.shape[1] - 1
+        for i in range(column_length):
+            series = table_for_graph.iloc[:, i+1]
+            dataset = {
+                "label": series.name,
+                "data": list(series),
+                "backgroundColor": colors[i],
+                "Color": colors[i],
+                "borderColor": colors[i]
+            }
+            datasets.append(dataset)
+        datasets = json.dumps(datasets)
+        # context로 넘겨주기
+        context = {
+            "labels": labels,
+            "datasets": datasets
+        }
+        return render(request, 'ubno_study/my_study_time.html', context=context)
